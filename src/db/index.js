5bdace00776558927d9f4fec00de4d95d9be005c
Adityagupta1947 <aditya.22207@knit.ac.in>
2025-04-07 04:50:31 +0530

    check consoles commented

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,10 @@
-import mongoose from "mongoose"
-import { DB_NAME } from "../constants"
+import mongoose, { Mongoose } from "mongoose"
+import { DB_NAME } from "../constants.js"
 
 async function DB_Connect(){
     try {
     const connection_obj = await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
-    console.log("\n",connection_obj)
+    // console.log("\n",connection_obj===mongoose)   // connection obj is same as imported mongoose but not the database obj
     console.log("\n","Connected!! DB_HOST:",connection_obj.connection.host)
     } catch (error) {
         console.log("Database Connection Failed! Error:",error)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import dotenv from "dotenv"
 import express from "express"
+import DB_Connect from "./db/index.js"
 
 dotenv.config({
     path:"./.env"
 })
+DB_Connect()
+
+const app=express()
+
+app.get("/",(req,res)=>{
+    res.send("database is connected successfully")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is listening at port:",process.env.PORT)
+})
+
 
